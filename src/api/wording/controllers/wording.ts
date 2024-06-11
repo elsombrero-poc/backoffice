b249fd7b69d48a9025e932a082203a8dbff3791b
Nirilala Rakotondrasoa <nirilala15@gmail.com>
2024-06-11 20:19:22 +0200

    feat: initial commit

---
+++ src/api/wording/controllers/wording.ts
@@ -0,0 +1,7 @@
+/**
+ * wording controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::wording.wording');
---
+++ src/api/wording/routes/wording.ts
@@ -0,0 +1,7 @@
+/**
+ * wording router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::wording.wording');
